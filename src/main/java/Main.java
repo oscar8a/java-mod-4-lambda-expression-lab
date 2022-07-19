4932e95b4c66d458d5a0a3c53c197729f6a8522b
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-19 14:33:06 -0400

    nextOddNum lambda expression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,28 @@ import java.util.function.IntUnaryOperator;
 
 public class Main {
 	// write the nextOddNum here
-    
+    public static NumberLambda nextOddNum = new NumberLambda() {
+        @Override
+        public int applyAsInt(int input) {
+            // If even, return the number + 1, else recursively call with the value + 1 to check the next number
+            return input % 2 == 0 ? input + 1 : applyAsInt(input + 1);
+        }
+    };
+
     public static void main(String[] args) {
         int num = 5;
         int num2 = 10;
+        int num3 = 17;
+        int num4 = 50;
 
         System.out.println(nextOddNum.applyAsInt(num)); // 7
         System.out.println(nextOddNum.applyAsInt(num2)); // 11
+        System.out.println(nextOddNum.applyAsInt(num3)); // 19
+        System.out.println(nextOddNum.applyAsInt(num4)); // 51
     }
+}
+
+@FunctionalInterface
+interface NumberLambda {
+    int applyAsInt(int input);
 }
